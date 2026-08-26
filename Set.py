4e04f22bca1b633f731e8c08b3a5353b82491bd1
Akshadabhosale016 <anitabhosale789@gmail.com>
2026-08-26 10:53:10 +0530

    Added set practice

---
+++ Set.py
@@ -0,0 +1,55 @@
+# Sets
+# Program 1
+'''numbers = {10, 20, 30}
+numbers.add(40)      # add()
+print(numbers)
+
+# program 2
+numbers = {10, 20, 30, 40}
+numbers.remove(20)   # remove()
+print(numbers)
+
+# Program 3
+A = {1, 2, 3}
+B = {3, 4, 5}
+print(A.union(B)) # output {1,2,3,4,5}
+
+# Program 4
+A = {10, 20, 30, 40}
+B = {30, 40, 50, 60}
+print(A.intersection(B))  # output {30,40}
+
+# Program 5
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7}
+print(A.difference(B)) # output  { 1,2,3}
+
+
+# Program 6
+A = {10, 20, 30, 40}
+B = {30, 40, 50, 60}
+print(A.symmetric_difference(B)) #output  {10,20,50,60}
+
+# Program 7
+fruits = {"apple", "banana", "mango"}
+print("banana" in fruits)          # output :True
+print("orange" not in fruits)      # output :True
+
+# Program 8
+numbers = {10, 15, 20, 25, 30, 35}
+for num in numbers:
+    if num % 2 == 0:
+        print(num)
+
+# Program 9
+numbers = {1, 2, 3, 4, 5, 6}
+square = { num*num for num in numbers}
+print(square)
+
+# Program 10
+numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+square = {num*num for num in numbers if num % 2 != 0}
+print(square)'''
+
+
+
